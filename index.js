b702c6068c9ed952e7713e9f87678f298fc49b17
Soma-avi <SarahSoma@protonmail.com>
2022-02-19 17:25:14 +0300

    Added with Episode 3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
-const generateImage = require("./generateImage")
+// const generateImage = require("./generateImage")
 
 const client = new Discord.Client({
     intents: [
@@ -11,24 +11,44 @@ const client = new Discord.Client({
     ]
 })
 
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}`)
-})
+let bot = {
+    client,
+    prefix: "s-",
+    owners : ["253837139031097344"]
+}
 
-client.on("messageCreate", (message) => {
-    if (message.content == "hi"){
-        message.reply("Hello World!")
-    }
-})
+client.slashcommands = new Discord.Collection()
+client.commands = new Discord.Collection()
+client.events = new Discord.Collection()
 
-const welcomeChannelId = "809123635922599937"
+client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload)
+client.loadCommands = (bot, reload) => require("./handlers/commands")(bot, reload)
+client.loadSlashCommands = (bot, reload) => require("./handlers/slashcommands")(bot, reload)
 
-client.on("guildMemberAdd", async (member) =>{
-    const img = await generateImage(member)
-    member.guild.channels.cache.get(welcomeChannelId).send({
-        content: `<@${member.id}> Welcome to the server!`,
-        files: [img]
-    })
-})
+client.loadEvents(bot, false)
+client.loadCommands(bot, false)
+client.loadSlashCommands(bot, false)
+
+module.exports = bot
+
+// client.on("ready", () => {
+//     console.log(`Logged in as ${client.user.tag}`)
+// })
+
+// client.on("messageCreate", (message) => {
+//     if (message.content == "hi"){
+//         message.reply("Hello World!")
+//     }
+// })
+
+// const welcomeChannelId = "809123635922599937"
+
+// client.on("guildMemberAdd", async (member) =>{
+//     const img = await generateImage(member)
+//     member.guild.channels.cache.get(welcomeChannelId).send({
+//         content: `<@${member.id}> Welcome to the server!`,
+//         files: [img]
+//     })
+// })
 
 client.login(process.env.TOKEN)
