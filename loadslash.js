cf671cc95cba46b313524d52ebd7a851588c7bdf
Somavi <SarahSoma@protonmail.com>
2022-06-14 23:08:06 +0300

    Refresh

---
+++ loadslash.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js")
+require("dotenv").config()
+
+// const generateImage = require("./generateImage")
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
+    ]
+})
+
+let bot = {
+    client,
+    prefix: "s-",
+    owners : ["253837139031097344"]
+}
+
+client.slashcommands = new Discord.Collection()
+client.commands = new Discord.Collection()
+client.events = new Discord.Collection()
+
+client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload)
+client.loadCommands = (bot, reload) => require("./handlers/commands")(bot, reload)
+client.loadSlashCommands = (bot, reload) => require("./handlers/slashcommands")(bot, reload)
+
+client.loadEvents(bot, false)
+client.loadCommands(bot, false)
+client.loadSlashCommands(bot, false)
+
+module.exports = bot
+
+client.login(process.env.TOKEN)
